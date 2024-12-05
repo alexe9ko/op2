680bd243dae1ef19b86d5634e865f9dadcba44fb
alexe <alexe9ko9@gmail.com>
2024-12-05 07:13:49 +0300

    Циклы

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ i >= -10; i = i - 1) {
                 for (
 int i = 1904;
 i <= 2096; i = i + 4) {
-        System.out.println(+i + " год является високосным");
+        System.out.println(+i + " год является високосным.");
         }
 
                 for (
