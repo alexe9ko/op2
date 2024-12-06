f31d418eb2d3cfc3553f77f4ab5571700535f4c0
alexe <alexe9ko9@gmail.com>
2024-12-06 07:53:41 +0300

    Циклы

--- src/Main.java
+++ src/Main.java
@@ -3,64 +3,64 @@ public class Main {
         System.out.println("Hello world!");
 
 
-for (
-int i = 0;
-i <= 10; i++) {
-        System.out.println(+i);
-        }
-
-                for (
-int i = 10;
-i >= 0; i--) {
-        System.out.println(+i);
-        }
-                for (
-int i = 0;
-i < 17; i = i + 2) {
-        System.out.println(+i);
-        }
-
-                for (
-int i = 10;
-i >= -10; i = i - 1) {
-        System.out.println(+i);
-        }
-
-                for (
-int i = 1904;
-i <= 2096; i = i + 4) {
-        System.out.println(+i + " год является високосным.");
-        }
-
-                for (
-int i = 7;
-i <= 98; i = i + 7) {
-        System.out.println(+i);
-        }
-
-                for (
-int i = 1;
-i <= 512; i = i * 2) {
-        System.out.println(+i);
-        }
-
-int salary = 29000;
-int tatal = 0;
         for (
-int i = 1;
-i <= 12; i++) {
-tatal = tatal + tatal / 100;
-tatal = tatal + salary;
+                int i = 0;
+                i <= 10; i++) {
+            System.out.println(+i);
+        }
+
+        for (
+                int i = 10;
+                i >= 0; i--) {
+            System.out.println(+i);
+        }
+        for (
+                int i = 0;
+                i < 17; i = i + 2) {
+            System.out.println(+i);
+        }
+
+        for (
+                int i = 10;
+                i >= -10; i = i - 1) {
+            System.out.println(+i);
+        }
+
+        for (
+                int i = 1904;
+                i <= 2096; i = i + 4) {
+            System.out.println(+i + " год является високосным.");
+        }
+
+        for (
+                int i = 7;
+                i <= 98; i = i + 7) {
+            System.out.println(+i);
+        }
+
+        for (
+                int i = 1;
+                i <= 512; i = i * 2) {
+            System.out.println(+i);
+        }
+
+        int salary = 29000;
+        int tatal = 0;
+        for (
+                int i = 1;
+                i <= 12; i++) {
+            tatal = tatal + tatal / 100;
+            tatal = tatal + salary;
             System.out.println(" месяц " + i + ", сумма накоплений " + tatal);
 
         }
 
-int tab = 0;
+        int tab = 0;
         for (
-int i = 1;
-i <= 10; i = i + 1) {
-tab = tab + 2;
-        System.out.println("2*" + i + "=" + tab);
+                int i = 1;
+                i <= 10; i = i + 1) {
+            tab = tab + 2;
+            System.out.println("2*" + i + "=" + tab);
         }
-                }
-                }
+    }
+}
